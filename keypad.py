64e8093e51d42d1e60140d19bea1f82e936c2d18
Manuel Esberger <esberger.manuel@live.at>
2018-02-03 19:57:01 +0100

    e

--- keypad.py
+++ keypad.py
@@ -8,32 +8,32 @@ class Keypad:
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
 
-        MATRIX = [[1, 2, 3, "A"],
+        self.MATRIX = [[1, 2, 3, "A"],
                   [4, 5, 6, "B"],
                   [7, 8, 9, "C"],
                   ['*', 0, '#', 'D']]
 
-        ROW = [7, 11, 13, 15]
-        COL = [12, 16, 18, 22]
+        self.ROW = [7, 11, 13, 15]
+        self.COL = [12, 16, 18, 22]
 
         for j in range(4):
-            GPIO.setup(COL[j], GPIO.OUT)
-            GPIO.output(COL[j], 1)
+            GPIO.setup(self.COL[j], self.GPIO.OUT)
+            GPIO.output(self.COL[j], 1)
 
         for i in range(4):
-            GPIO.setup(ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.setup(self.ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def read():
         try:
             for j in range(4):
-                GPIO.output(COL[j], 0)
+                GPIO.output(self.COL[j], 0)
 
                 for i in range(4):
-                    if GPIO.input(ROW[i]) == 0:
-                        print(MATRIX[i][j])
-                        while (GPIO.input(ROW[i]) == 0):
+                    if GPIO.input(self.ROW[i]) == 0:
+                        print(self.MATRIX[i][j])
+                        while (self.GPIO.input(ROW[i]) == 0):
                             pass
-                GPIO.output(COL[j],1)
+                GPIO.output(self.COL[j],1)
         except KeyboardInterrupt:
             GPIO.cleanup()
 
@@ -41,3 +41,4 @@ class Keypad:
 while(True):
     keypad = Keypad()
     keypad.read()
+    time.sleep(1)
