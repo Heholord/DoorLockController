51570e5a679eddf56b98b700613c5e8baebc472c
Manuel Esberger <esberger.manuel@live.at>
2018-02-03 20:48:00 +0100

    abc

--- KeypadPassChecker.py
+++ KeypadPassChecker.py
@@ -3,30 +3,8 @@ import myfile
 
 
 class Keypad_pass_checker:
-
-    class Input:
-        def __init__(self):
-            self.keypad = Keypad()
-
-        def read(self):
-            self.trial = ""
-            while(self.keypad.read != "#"):
-                self.trial += keypad.read()
-                print(self.trial)
-            return self.trial
-
-        def get_input(self):
-            return self.trial
-
-    class Checker:
-        def __init__(self):
-            self.solution = myfile.readfile("test")
-
-        def compare(self, input):
-            return self.solution == input.get_input()
-
     def auth(self):
-        inputt = Input()
+        inputt = KeyInput()
         checker = Check()
 
         inputt.read()
@@ -34,6 +12,29 @@ class Keypad_pass_checker:
             print("access granted")
         else:
             print("sorry no")
+
+
+class Input:
+    def __init__(self):
+        self.keypad = Keypad()
+
+    def read(self):
+        self.trial = ""
+        while(self.keypad.read != "#"):
+            self.trial += keypad.read()
+            print(self.trial)
+        return self.trial
+
+    def get_input(self):
+        return self.trial
+
+
+class Checker:
+    def __init__(self):
+        self.solution = myfile.readfile("test")
+
+    def compare(self, input):
+        return self.solution == input.get_input()
 
 checker = Keypad_pass_checker()
 checker.auth()
