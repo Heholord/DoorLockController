7b6ac46c99cfa7c89fbe70232a8b4aa422b0c04b
Manuel Esberger <esberger.manuel@live.at>
2018-02-04 00:20:27 +0100

    asb

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 checker = Keypad_pass_checker()
 door = Door()
 if checker.auth():
-    for i in range(0, 50):
+    for i in range(0, 20):
         door.open_door()
-        time.sleep(0.1)
+        time.sleep(0.15)
         door.close_door()
