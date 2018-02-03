639bd94ed184cbd9d3c85b17d93856d729a5b4d3
Manuel Esberger <esberger.manuel@live.at>
2018-02-04 00:13:28 +0100

    asb

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from keypad.KeypadPassChecker import Keypad_pass_checker
 from door.Door import Door
+import time
 
 checker = Keypad_pass_checker()
 door = Door()
 if checker.auth():
     door.open_door()
-    sleep()
+    time.sleep(3)
     door.close_door()
