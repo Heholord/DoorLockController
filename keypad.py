58d1cfe493850dc4909f0f184485270794128989
Manuel Esberger <esberger.manuel@live.at>
2018-02-03 19:52:32 +0100

    abc

--- keypad.py
+++ keypad.py
@@ -1,37 +1,42 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode (GPIO.BOARD)
-GPIO.setwarnings(False)
 
-MATRIX = [[1,2,3,"A"],
-           [4,5,6,"B"],
-           [7,8,9,"C"],
-           ['*',0,'#','D']]
+class Keypad:
+    def __init__(self):
+        self.data = []
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
 
-ROW =   [7,11,13,15]
-COL = [12,16,18,22]
+        MATRIX = [[1, 2, 3, "A"],
+                  [4, 5, 6, "B"],
+                  [7, 8, 9, "C"],
+                  ['*', 0, '#', 'D']]
 
-for j in range(4):
-    GPIO.setup(COL[j], GPIO.OUT)
-    GPIO.output(COL[j], 1)
+        ROW = [7, 11, 13, 15]
+        COL = [12, 16, 18, 22]
 
-for i in range (4):
-    GPIO.setup(ROW[i], GPIO.IN, pull_up_down = GPIO.PUD_UP)
+        for j in range(4):
+            GPIO.setup(COL[j], GPIO.OUT)
+            GPIO.output(COL[j], 1)
 
-try:
-    while(True):
-        for j in range (4):
-            GPIO.output(COL[j],0)
+        for i in range(4):
+            GPIO.setup(ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-            for i in range(4):
-                if GPIO.input (ROW[i]) == 0:
-                    print(MATRIX[i][j])
-                    time.sleep(1)
-                    while (GPIO.input(ROW[i]) == 0):
-                        pass
+    def read():
+        try:
+            for j in range(4):
+                GPIO.output(COL[j], 0)
 
-            GPIO.output(COL[j],1)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+                for i in range(4):
+                    if GPIO.input(ROW[i]) == 0:
+                        print(MATRIX[i][j])
+                        while (GPIO.input(ROW[i]) == 0):
+                            pass
+                GPIO.output(COL[j],1)
+        except KeyboardInterrupt:
+            GPIO.cleanup()
 
+
+while(True):
+    read()
